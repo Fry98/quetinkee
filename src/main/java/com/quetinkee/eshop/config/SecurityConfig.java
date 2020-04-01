9281d52d6f6e0d8ec68e9b411c04a05e1e3d2ad6
David Krejci <krejcda1@fel.cvut.cz>
2020-04-01 20:47:35 +0200

    Secure login stub test

---
+++ src/main/java/com/quetinkee/eshop/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.quetinkee.eshop.config;
+
+/**
+* Just testing for login - will be removed / edited later 
+*  TO pom.xml
+    <dependency>
+      <groupId>org.springframework.security</groupId>
+      <artifactId>spring-security-config</artifactId>
+    </dependency>
+    
+    <dependency>
+      <groupId>org.springframework.security</groupId>
+      <artifactId>spring-security-web</artifactId>
+    </dependency>
+*/
+/*
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// password
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+      .withUser("user").password(passwordEncoder().encode("heslo")).roles("ADMIN");
+  }
+
+  @Override
+  protected void configure(final HttpSecurity http) throws Exception {
+    http
+      .csrf().disable()
+      .authorizeRequests()
+      .antMatchers("/admin/**").hasRole("ADMIN")
+      .anyRequest().permitAll()
+      .and()
+      .formLogin()
+      .loginPage("/login")
+      .loginProcessingUrl("/api/login")
+      .usernameParameter("mail").passwordParameter("pwd")
+      .defaultSuccessUrl("/login?success=true", true)
+      .failureUrl("/login?error=true")
+      .and()
+      .logout()
+      .logoutUrl("/api/logout")
+      .logoutSuccessUrl("/logout");
+  }
+  
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
+*/
