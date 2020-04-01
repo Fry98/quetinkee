1879f324eb505da8fa1400c9f74496f1afae81ea
David Krejci <krejcda1@fel.cvut.cz>
2020-04-02 00:15:52 +0200

    JSON response

--- src/main/java/com/quetinkee/eshop/config/SecurityConfig.java
+++ src/main/java/com/quetinkee/eshop/config/SecurityConfig.java
@@ -2,8 +2,13 @@ package com.quetinkee.eshop.config;
 
 /**
 * Just testing for login - will be removed / edited later 
+* 
+* notes:
+//      .accessDeniedHandler(authentificationHandler)\
+// response.sendError(response.getStatus(), "todo"); not implemented yet
 */
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,14 +17,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 // password
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+// authentificator
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  private AuthentificationHandler authentificationHandler;
+  
   @Override
   protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
@@ -38,16 +58,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .loginPage("/login")
       .loginProcessingUrl("/api/login")
       .usernameParameter("mail").passwordParameter("pwd")
-      .defaultSuccessUrl("/login?success=true", true)
-      .failureUrl("/login?error=true")
+      .successHandler(authentificationHandler)
+      .failureHandler(authentificationHandler)
       .and()
       .logout()
       .logoutUrl("/api/logout")
-      .logoutSuccessUrl("/logout");
+      .logoutSuccessHandler(authentificationHandler);
   }
   
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+}
+
+@Service
+class AuthentificationHandler implements LogoutSuccessHandler, AuthenticationFailureHandler, AuthenticationSuccessHandler {
+
+  @Override
+  public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication a) throws IOException, ServletException {
+    this.setResponse(response, HttpStatus.OK.value(), "Logout success");
+  }
+
+  @Override
+  public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException ae) throws IOException, ServletException {
+    this.setResponse(response, HttpStatus.FORBIDDEN.value(), "Login failed");
+  }
+
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication a) throws IOException, ServletException {
+    this.setResponse(response, HttpStatus.OK.value(), "Login success");
+  }
+
+  private void setResponse (HttpServletResponse response, int code, String message) throws IOException {
+    response.setContentType("application/json;charset=UTF-8");
+    response.setStatus(code);
+    response.getWriter().write("{\"message\": \"" + message + "\"}");
+    response.getWriter().flush();    
   }
 }
