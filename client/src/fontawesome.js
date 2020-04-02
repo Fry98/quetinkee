51fb16b624b1f1ac863806b638c7ac3114196044
vitriha <vit.riha@aspectworks.com>
2020-04-02 15:11:19 +0200

    add productTile component

--- client/src/fontawesome.js
+++ client/src/fontawesome.js
@@ -4,10 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import {} from '@fortawesome/free-regular-svg-icons';
 import {
   faUser,
-  faShoppingCart
+  faShoppingCart,
+  faCartPlus
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faUser);
 library.add(faShoppingCart);
+library.add(faCartPlus);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,31 @@
 <template>
-  <div id="home">
-    Homepage
+  <div id='home'>
+    <h1>Nové</h1>
+    <ProductTile name='Nazev kytice' price='899'></ProductTile>
+    <h1>V Akci</h1>
+    <ProductTile name='Nazev kytice' price='899'></ProductTile>
+    <h1>Doporučené</h1>
+    <ProductTile name='Nazev kytice' price='899'></ProductTile>
   </div>
 </template>
 
 <script>
+import ProductTile from '../components/ProductTile';
 export default {
-  name: 'home'
+  name: 'home',
+  components: {ProductTile}
 };
 </script>
+
+<style lang='scss' scoped>
+  #home {
+    padding: 1em 2em;
+  }
+
+  h1 {
+    margin: 1em 0 0 0;
+    &:first-child {
+      margin: 0;
+    }
+  }
+</style>
