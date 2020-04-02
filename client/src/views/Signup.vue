3ad887e7c305c811d4120e12b5c0f3a038c381c7
Fry98 <filip-toman@seznam.cz>
2020-04-02 16:56:52 +0200

    Login request from Axios

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -14,6 +14,11 @@
             <general-input title='Heslo' type='password' v-model='pwd'></general-input>
             <general-input title='Heslo znovu' type='password' v-model='rePwd'></general-input>
             <div id='button-wrap'>
+              <button class='button step back-btn' @click="$router.push('/login')">
+                <font-awesome-icon icon='chevron-left'></font-awesome-icon>
+                <span>Zpět</span>
+              </button>
+              <div id='filler'></div>
               <button class='button step' @click="nextStep">
                 <font-awesome-icon icon='chevron-right'></font-awesome-icon>
                 <span>Pokračovat</span>
@@ -25,7 +30,7 @@
             <general-input title='Město' v-model='city'></general-input>
             <general-input title='Ulice, č.p.' v-model='street'></general-input>
             <general-input title='PSČ' v-model='zip'></general-input>
-            <div>
+            <div id='adr-check'>
               <label>
                 <input type='checkbox' v-model='billingIsSame'>
                 Doručovací adresa je shodná s fakturační adresou
@@ -162,11 +167,20 @@ export default {
   }
 
   #button-wrap {
-    text-align: right;
+    justify-content: flex-end;
+    display: flex;
   }
 
   #end-buttons {
     display: flex;
     margin-top: 15px;
+  }
+
+  #filler {
+    flex: 1;
+  }
+
+  #adr-check {
+    margin-bottom: 10px;
   }
 </style>
