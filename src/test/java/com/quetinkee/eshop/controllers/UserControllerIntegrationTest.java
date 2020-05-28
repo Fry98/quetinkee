6f90d9904fb0be78dfb96bcf768c6d84951432b0
Timur Korolev <koroltim@fel.cvut.cz>
2020-05-28 16:17:40 +0200

    Pridani velkeho poctu testu

--- src/test/java/com/quetinkee/eshop/controllers/UserControllerIntegrationTest.java
+++ src/test/java/com/quetinkee/eshop/controllers/UserControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.quetinkee.eshop.controllers;
 
+import com.quetinkee.eshop.model.Address;
 import com.quetinkee.eshop.model.User;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,4 +44,171 @@ public class UserControllerIntegrationTest {
         assertEquals("hersosi2002" , response.getBody().getPassword());
         assertEquals("123456789" , response.getBody().getPhone());
     }
+
+    @Test
+    public void updateUserTest(){
+        UserController usrCntrl = new UserController();
+        //User newUser = new User("Zhigalo" , "Ebanoye" , "ebazhi@yoba.com" , "hersosi2002" , "123456789");
+        User newUser = new User();
+        newUser.setFirstName("Zhigalo");
+        newUser.setLastName("Ebanoye");
+        newUser.setMail("ebazhi@yoba.com");
+        newUser.setPassword("hersosi2002");
+        newUser.setPhone("123456789");
+        //HttpEntity<User> request = new HttpEntity<>(newUser);
+        //ResponseEntity<User> response = testRestTemplate.postForEntity("/user" , request , User.class);
+
+        ResponseEntity<User> response = usrCntrl.create(newUser);
+
+        User updateFrom = new User("Pidrilo" , "Ebanoye" , "ebapidr@yoba.com" , "sosipisyu2002" , "123456788");
+
+        User updatedUser = usrCntrl.updateId(response.getBody().getId() , updateFrom );
+
+        assertNotNull(updatedUser);
+        assertEquals("Pidrilo" , updatedUser.getFirstName());
+        assertEquals("Ebanoye" , updatedUser.getLastName());
+        assertEquals("ebapidr@yoba.com" , updatedUser.getMail());
+        assertEquals("sosipisyu2002" , updatedUser.getPassword());
+        assertEquals("123456788" , updatedUser.getPhone());
+
+    }
+
+    @Test
+    public void deleteUserTest(){
+        UserController usrCntrl = new UserController();
+        //User newUser = new User("Zhigalo" , "Ebanoye" , "ebazhi@yoba.com" , "hersosi2002" , "123456789");
+        User newUser = new User();
+        newUser.setFirstName("Zhigalo");
+        newUser.setLastName("Ebanoye");
+        newUser.setMail("ebazhi@yoba.com");
+        newUser.setPassword("hersosi2002");
+        newUser.setPhone("123456789");
+        //HttpEntity<User> request = new HttpEntity<>(newUser);
+        //ResponseEntity<User> response = testRestTemplate.postForEntity("/user" , request , User.class);
+
+        ResponseEntity<User> response = usrCntrl.create(newUser);
+
+        usrCntrl.deleteId(response.getBody().getId());
+
+        assertNull(usrCntrl.getId(response.getBody().getId()));
+    }
+
+    @Test
+    public void createBillingAdressTest(){
+        UserController usrCntrl = new UserController();
+        //User newUser = new User("Zhigalo" , "Ebanoye" , "ebazhi@yoba.com" , "hersosi2002" , "123456789");
+        User newUser = new User();
+        newUser.setFirstName("Zhigalo");
+        newUser.setLastName("Ebanoye");
+        newUser.setMail("ebazhi@yoba.com");
+        newUser.setPassword("hersosi2002");
+        newUser.setPhone("123456789");
+        //HttpEntity<User> request = new HttpEntity<>(newUser);
+        //ResponseEntity<User> response = testRestTemplate.postForEntity("/user" , request , User.class);
+
+        ResponseEntity<User> response = usrCntrl.create(newUser);
+
+        Address addr = new Address();
+        addr.setCity("Praha");
+        addr.setStreet("Na Lysine");
+        addr.setZip("15200");
+
+        usrCntrl.createAddressBillingId(response.getBody().getId() , addr);
+
+        assertNotNull(response.getBody().getAddressBilling());
+    }
+
+    @Test
+    public void createDeliverAdressTest(){
+        UserController usrCntrl = new UserController();
+        //User newUser = new User("Zhigalo" , "Ebanoye" , "ebazhi@yoba.com" , "hersosi2002" , "123456789");
+        User newUser = new User();
+        newUser.setFirstName("Zhigalo");
+        newUser.setLastName("Ebanoye");
+        newUser.setMail("ebazhi@yoba.com");
+        newUser.setPassword("hersosi2002");
+        newUser.setPhone("123456789");
+        //HttpEntity<User> request = new HttpEntity<>(newUser);
+        //ResponseEntity<User> response = testRestTemplate.postForEntity("/user" , request , User.class);
+
+        ResponseEntity<User> response = usrCntrl.create(newUser);
+
+        Address addr = new Address();
+        addr.setCity("Praha");
+        addr.setStreet("Na Lysine");
+        addr.setZip("15200");
+
+        usrCntrl.createAddressDeliveryId(response.getBody().getId() , addr);
+
+        assertNotNull(response.getBody().getAddressDelivery());
+    }
+
+    @Test
+    public void updateBillingAddressTest(){
+        UserController usrCntrl = new UserController();
+        //User newUser = new User("Zhigalo" , "Ebanoye" , "ebazhi@yoba.com" , "hersosi2002" , "123456789");
+        User newUser = new User();
+        newUser.setFirstName("Zhigalo");
+        newUser.setLastName("Ebanoye");
+        newUser.setMail("ebazhi@yoba.com");
+        newUser.setPassword("hersosi2002");
+        newUser.setPhone("123456789");
+        //HttpEntity<User> request = new HttpEntity<>(newUser);
+        //ResponseEntity<User> response = testRestTemplate.postForEntity("/user" , request , User.class);
+
+        ResponseEntity<User> response = usrCntrl.create(newUser);
+
+        Address addr = new Address();
+        addr.setCity("Praha");
+        addr.setStreet("Na Lysine");
+        addr.setZip("15200");
+
+        usrCntrl.createAddressBillingId(response.getBody().getId() , addr);
+
+        Address addre = new Address();
+        addr.setCity("Brno");
+        addr.setStreet("Super popa");
+        addr.setZip("1447");
+
+        usrCntrl.updateAddressBillingId(response.getBody().getId() , addre);
+
+        assertEquals(response.getBody().getAddressBilling().getCity(), addre.getCity());
+        assertEquals(response.getBody().getAddressBilling().getStreet(), addre.getStreet());
+        assertEquals(response.getBody().getAddressBilling().getZip() , addre.getZip());
+    }
+
+    @Test
+    public void updateDeliveryAddressTest(){
+        UserController usrCntrl = new UserController();
+        //User newUser = new User("Zhigalo" , "Ebanoye" , "ebazhi@yoba.com" , "hersosi2002" , "123456789");
+        User newUser = new User();
+        newUser.setFirstName("Zhigalo");
+        newUser.setLastName("Ebanoye");
+        newUser.setMail("ebazhi@yoba.com");
+        newUser.setPassword("hersosi2002");
+        newUser.setPhone("123456789");
+        //HttpEntity<User> request = new HttpEntity<>(newUser);
+        //ResponseEntity<User> response = testRestTemplate.postForEntity("/user" , request , User.class);
+
+        ResponseEntity<User> response = usrCntrl.create(newUser);
+
+        Address addr = new Address();
+        addr.setCity("Praha");
+        addr.setStreet("Na Lysine");
+        addr.setZip("15200");
+
+        usrCntrl.createAddressDeliveryId(response.getBody().getId() , addr);
+
+        Address addre = new Address();
+        addr.setCity("Brno");
+        addr.setStreet("Super popa");
+        addr.setZip("1447");
+
+        usrCntrl.updateAddressDeliveryId(response.getBody().getId() , addre);
+
+        assertEquals(response.getBody().getAddressDelivery().getCity(), addre.getCity());
+        assertEquals(response.getBody().getAddressDelivery().getStreet(), addre.getStreet());
+        assertEquals(response.getBody().getAddressDelivery().getZip() , addre.getZip());
+    }
+
 }
